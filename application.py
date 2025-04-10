176a1edf690e725b05505e6a6ced5a6faf30eaba
BIKRAM KUMAR JHA <140577657+Bikramkumarjha@users.noreply.github.com>
2025-04-10 14:09:12 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, app,render_template
+from flask import Flask, request,jsonify,app,render_template
 from flask import Response
 import pickle
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 application = Flask(__name__)
 app=application
 
-scaler=pickle.load(open("Model/standardScalar.pkl", "rb"))
-model = pickle.load(open("Model/modelForPrediction.pkl", "rb"))
+scaler=pickle.load(open("/solar_power_scaler.pkl", "rb"))
+model = pickle.load(open("/solar_power_model.pkl", "rb"))
 
 ## Route for homepage
 
@@ -19,33 +20,37 @@ def index():
 
 ## Route for Single data point prediction
 @app.route('/predictdata',methods=['GET','POST'])
-def predict_datapoint():
-    result=""
-
+def predict_datapoint()
     if request.method=='POST':
+        temperature=int(request.form.get("temperature"))
+        Relative_Humidity = float(request.form.get("Relative Humidity"))
+        mean_sea_level_pressure= float(request.form.get("mean sea level pressure"))
+        total_precipitation = float(request.form.get(" total precipitation"))
+        snowfall_amount= float(request.form.get("snowfall amount"))
+        total_cloud_cover = float(request.form.get(" total cloud cover"))
+        high_cloud_cover_high_cld_lay = float(request.form.get("high cloud cover high cld lay"))
+        medium_cloud_cover_mid_cld_lay = float(request.form.get("medium cloud cover mid cld lay"))
+        low_cloud = float(request.form.get("low cloud cover low cld lay"))
+        shortwave_radiation_backwards = float(request.form.get("shortwave radiation backwards"))
+        wind_speed_10m= float(request.form.get(" wind speed 10 m above gnd "))
+        wind_direction_10m = float(request.form.get("wind direction 10 m above gnd "))
+        wind_speed_80m = float(request.form.get("  wind speed 80 m abovr gnd"))
+        wind_direction_80m = float(request.form.get(" wind direction 80 m above gnd "))
+        wind_speed_900mb = float(request.form.get(" wind_speed_900_mb"))
+        wind_direction_900mb = float(request.form.get(" wind_direction 900_mb"))
+        wind_gust_10m_above_gnd = float(request.form.get("  wind_gust_10m_above_gnd ")) 
+        angle_of_incidence = float(request.form.get(" angle_of_incidence ")) 
+        zenith = float(request.form.get("  zenith ")) 
+        azimuth = float(request.form.get(" azimuth"))
+        generated_power_kW = float(request.form.get(" generation_power_kw ")) 
 
-        Pregnancies=int(request.form.get("Pregnancies"))
-        Glucose = float(request.form.get('Glucose'))
-        BloodPressure = float(request.form.get('BloodPressure'))
-        SkinThickness = float(request.form.get('SkinThickness'))
-        Insulin = float(request.form.get('Insulin'))
-        BMI = float(request.form.get('BMI'))
-        DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
-        Age = float(request.form.get('Age'))
 
-        new_data=scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
-        predict=model.predict(new_data)
-       
-        if predict[0] ==1 :
-            result = 'Diabetic'
-        else:
-            result ='Non-Diabetic'
+        new_data=scaler.transform([[temperature, Relative_Humidity,mean_sea_level_pressure, total_precipitation,snowfall_amount,total_cloud_cover,high_cloud_cover_high_cld_lay,medium_cloud_cover_mid_cld_lay,low_cloud ,shortwave_radiation_backwards,wind_speed_10m, wind_direction_10m, wind_speed_80m, wind_speed_900mb, wind_direction_900mb, wind_gust_10m_above_gnd,angle_of_incidence,zenith,azimuth,generated_power_kW]])
+        result=model.predict(new_data)
             
-        return render_template('single_prediction.html',result=result)
+         return render_template('home.html',result=result[0])
 
     else:
         return render_template('home.html')
-
-
 if __name__=="__main__":
     app.run(host="0.0.0.0")
